2f31fded2e895bed4becf42793d44562e019693e
Egor <e.r.ivanov@inbox.ru>
2022-04-21 16:01:47 +0400

    first commit

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -12,7 +12,23 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseIterative() {
-    // Напишите здесь свой код ...
+    if (size_ < 2){
+      return;
+    }
+    Node* head = front_;
+    Node* next_elem = front_->next;
+    Node* help_elem = head->next->next;
+    while (help_elem != nullptr){
+      next_elem->next = head;
+      head = next_elem;
+      next_elem = help_elem;
+      help_elem = help_elem->next;
+    }
+    next_elem->next = head;
+    front_->next = nullptr;
+    help_elem = front_;
+    front_ = next_elem;
+    back_ = help_elem;
   }
 
   /**
@@ -23,12 +39,21 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseRecursive() {
-    // Напишите здесь свой код ...
+    if (size_ < 2) {
+      return;
+    }
+    LinkedList::reverse_recursive_helper(front_, nullptr);
+    Node* help_elem = front_;
+    front_ = back_;
+    back_ = help_elem;
   }
 
   // вспомогательный метод для реализации рекурсии
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
-    // Напишите здесь свой код ...
+    if (curr->next != nullptr) {
+      LinkedList::reverse_recursive_helper(curr->next, curr);
+    }
+    curr->next = prev;
   }
 
 }  // namespace assignment
